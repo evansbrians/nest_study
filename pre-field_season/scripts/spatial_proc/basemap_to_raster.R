762b4aea13c6645d592bdc7a8ef4fd67169747e9
Brian Evans <evansbr@si.edu>
2026-05-18 13:09:10 -0400

    new script for generating background images for nest searching maps

---
+++ pre-field_season/scripts/spatial_proc/basemap_to_raster.R
@@ -0,0 +1,112 @@
+
+# setup -------------------------------------------------------------------
+
+library(maptiles)
+library(tmap)
+library(sf)
+library(tidyverse)
+
+# Get patches
+
+patches <-
+  st_read("data/spatial/patches.geojson", quiet = TRUE) %>% 
+  
+  # Use 3857 (Web Mercator):
+  
+  st_transform(3857)
+
+# Get a bounding box around all of the patches and make it a little bigger:
+
+patches_bbox <-
+  patches %>% 
+  st_buffer(100) %>% 
+  st_bbox() %>% 
+  st_as_sfc()
+
+# raster and raster processing --------------------------------------------
+
+# Get the full-color raster from ESRI for each patch
+
+photo_list <-
+  patches %>% 
+  st_buffer(50) %>% 
+  split(.$name) %>% 
+  map(
+    ~ maptiles::get_tiles(
+      x = .x,
+      zoom = 19,
+      provider = "Esri.WorldImagery",
+      crop = TRUE
+    ) %>% 
+      terra::project("EPSG:4326")
+  )
+
+# Make grayscale (multiplies each band with the value and sums bands):
+
+photo_list_grayscale <-
+  photo_list %>% 
+  map(
+    ~ .x %>% 
+      sum(
+        c(
+          0.299,
+          0.587,
+          0.114
+        )
+      )
+  )
+
+# write to file -----------------------------------------------------------
+
+photo_list_grayscale %>% 
+  imap(
+    \(.x, .name) {
+      writeRaster(
+        .x,
+        file.path(
+          "data/spatial/aerial_images",
+          str_c(.name, ".tif")
+        )
+      )
+    }
+  )
+
+# how to use --------------------------------------------------------------
+
+tmap_mode("plot")
+
+# Read in the images:
+
+background_images <-
+  list.files(
+    "data/spatial/aerial_images",
+    full.names = TRUE
+  ) %>% 
+  set_names(
+    str_remove(
+      basename(.),
+      ".tif"
+    )
+  ) %>% 
+  map(
+    ~ terra::rast(.x)
+  )
+
+# Map'em:
+
+background_images %>% 
+  pluck("grassland_b_fence") %>% 
+  tm_shape() +
+  tm_raster(
+    col.scale = 
+      tm_scale_continuous(values = "gray"),
+    col.legend = tm_legend_hide(),
+    col_alpha = 0.5
+  ) +
+  patches %>% 
+  tm_shape() +
+  tm_polygons(fill_alpha = 0.2) +
+  tmap_options(
+    frame = FALSE,
+    outer.margins = rep(0, 4)
+  )
