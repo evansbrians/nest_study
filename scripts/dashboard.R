df459c6837ed9025e54ea10d1ee517dbf66fdece
Brian Evans <evansbr@si.edu>
2026-05-26 08:18:58 -0400

    changed order of items for updating workflow and added printing utility

--- scripts/dashboard.R
+++ scripts/dashboard.R
@@ -3,20 +3,15 @@
 
 # setup -------------------------------------------------------------------
 
+library(glue)
+library(here)
 library(tidyverse)
 
 googlesheets4::gs4_auth()
 
-# map print-outs ----------------------------------------------------------
-
-source("scripts/update_map_print-outs.R")
-
 # download data points from garmin ----------------------------------------
 
 source("scripts/convert_gpx_geojson.R")
-
-# print-outs --------------------------------------------------------------
-
 
 # read and pre-process data -----------------------------------------------
 
@@ -25,3 +20,29 @@ source("scripts/google_data_processing.R")
 # update google earth map -------------------------------------------------
 
 source("scripts/update_google_earth.R")
+
+# update leaflet map ------------------------------------------------------
+
+source("scripts/leaflet_map.R")
+
+# map print-outs ----------------------------------------------------------
+
+# Update:
+
+source("scripts/update_map_print-outs.R")
+
+# Print:
+
+list.files(
+  here("outputs/print-outs/patch_maps"),
+  pattern = "\\.png$",
+  full.names = TRUE
+) %>% 
+  walk(
+    ~ system(
+      glue("lp '{.x}'")
+    )
+  )
+
+
+
