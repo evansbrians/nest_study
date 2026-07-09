249a954db0f7abe01cd4f0281f1324f6575381a3
Brian Evans <evansbr@si.edu>
2026-07-09 06:59:09 -0400

    split functions and db explorer execution

--- scripts/db_data_explorer.R
+++ scripts/db_data_explorer.R
@@ -3,34 +3,18 @@
 
 library(DBI)
 library(RSQLite)
-library(dplyr)
 library(dbplyr)
-library(purrr)
-library(tibble)
+library(tidyverse)
+
+source("scripts/utils/functions/db_functions.R")
 
 # connection ---------------------------------------------------------------
 
 # Open a connection to the nest_study database:
 
-connect_nest_db <-
-  function(
-    .db_path = 
-      Sys.getenv(
-        "NEST_DB_PATH", 
-        unset = "nest_study.sqlite")
-  ) {
-    con <-
-      dbConnect(
-        RSQLite::SQLite(),
-        .db_path
-      )
-    dbExecute(con, "PRAGMA foreign_keys = ON;")
-    con
-  }
+con <- connect_nest_db()
 
 # Note: To list tables in the DB use:
-
-con <- connect_nest_db()
 
 dbListTables(con)
 
@@ -38,149 +22,44 @@ dbListTables(con)
 
 # Basic nest query:
 
-nest_query <-
-  function(.con) {
-    tbl(.con, "nest") %>%
-      left_join(
-        tbl(.con, "species"),
-        by = "species_code"
-      ) %>%
-      left_join(
-        tbl(.con, "patch"),
-        by = "patch_id"
-      ) %>%
-      arrange(nest_id)
-  }
+nest_query()
 
-# current + status ---------------------------------------------------------
+# Get the nests that are current:
 
-# Active "current" nests:
+get_current_nests()
 
-get_current_nests <-
-  function(.con) {
-    species_lookup <-
-      tbl(.con, "nest") %>%
-      left_join(
-        tbl(.con, "species"),
-        by = "species_code"
-      ) %>%
-      select(
-        nest_id,
-        species = common_name,
-        discovery_date
-      )
-    
-    tbl(.con, "v_current_nest") %>%
-      left_join(
-        species_lookup,
-        by = "nest_id"
-      ) %>%
-      arrange(
-        patch_id,
-        nest_id
-      )
-  }
+# Want to work with it as a tibble?
 
-# One nest's current status (discovery and interval):
+get_current_nests() %>% 
+  collect()
 
-get_nest_status <-
-  function(.con, .nest_id) {
-    tbl(.con, "v_nest_latest_check") %>%
-      filter(nest_id == .nest_id) %>%
-      left_join(
-        nest_query(.con),
-        by = "nest_id"
-      )
-  }
+# Discovery and interval data for a single nest:
+
+get_nest_status(.nest_id = "N119") %>% 
+  collect()
 
 # schedule -----------------------------------------------------------------
 
-schedule <-
-  function(.con, .week = NULL) {
-    days <- tbl(.con, "schedule_day")
-
-    if (!is.null(.week)) {
-      days <-
-        days %>%
-        filter(week == .week)
-    }
-    
-    days %>%
-      select(
-        week,
-        date,
-        day,
-        arrive,
-        patch_order,
-        patch_count,
-        boards,
-        check_nests,
-        predator_cameras,
-        notes
-      ) %>%
-      arrange(
-        date,
-        patch_order
-      ) %>%
-      collect()
-  }
+schedule(.week = 9)
 
 # verification -------------------------------------------------------------
 
 # Nests discovered on or after a date:
 
-nests_since <-
-  function(.con, .date) {
-    nest_query(.con) %>%
-      filter(discovery_date >= .date) %>%
-      arrange(
-        discovery_date,
-        nest_id
-      )
-  }
+nests_since(
+  .date = today() - 1
+)
 
 # Interval checks recorded on or after a date:
 
-checks_since <-
-  function(.con, .date) {
-    tbl(.con, "interval_check") %>%
-      filter(check_date >= .date) %>%
-      left_join(
-        nest_query(.con),
-        by = "nest_id"
-      ) %>%
-      arrange(
-        check_date,
-        nest_id
-      )
-  }
+checks_since(
+  .date = today() - 1
+)
 
 # Row counts for the core tables (a check to see if new data were added):
 
-data_counts <-
-  function(.con) {
-    tables <-
-      c(
-        "nest",
-        "interval_check",
-        "gps_point",
-        "nest_substrate",
-        "track"
-      )
-    tibble(
-      table = tables,
-      rows =
-        map_int(
-          tables,
-          function(.t) {
-            tbl(.con, .t) %>%
-              tally() %>%
-              pull(n)
-          }
-        )
-    )
-  }
+data_counts()
 
 # End connection each time you're done! -----------------------------------
 
-dbDisconnect()
+dbDisconnect(con)
---
+++ scripts/utils/functions/db_functions.R
@@ -0,0 +1,166 @@
+
+
+# utility functions -------------------------------------------------------
+
+# Open a connection to the nest_study database:
+
+connect_nest_db <-
+  function(
+    .db_path = 
+      Sys.getenv(
+        "NEST_DB_PATH", 
+        unset = "nest_study.sqlite")
+  ) {
+    con <-
+      dbConnect(
+        RSQLite::SQLite(),
+        .db_path
+      )
+    dbExecute(con, "PRAGMA foreign_keys = ON;")
+    con
+  }
+
+# nest queries ------------------------------------------------------------
+
+# Basic nest query:
+
+nest_query <-
+  function(.con = con) {
+    tbl(.con, "nest") %>%
+      left_join(
+        tbl(.con, "species"),
+        by = "species_code"
+      ) %>%
+      left_join(
+        tbl(.con, "patch"),
+        by = "patch_id"
+      ) %>%
+      arrange(nest_id)
+  }
+
+# Active "current" nests:
+
+get_current_nests <-
+  function(.con = con) {
+    species_lookup <-
+      tbl(.con, "nest") %>%
+      left_join(
+        tbl(.con, "species"),
+        by = "species_code"
+      ) %>%
+      select(
+        nest_id,
+        species = common_name,
+        discovery_date
+      )
+    
+    tbl(.con, "v_current_nest") %>%
+      left_join(
+        species_lookup,
+        by = "nest_id"
+      ) %>%
+      arrange(
+        patch_id,
+        nest_id
+      )
+  }
+
+# One nest's current status (discovery and most recent interval):
+
+get_nest_status <-
+  function(.con = con, .nest_id) {
+    tbl(.con, "v_nest_latest_check") %>%
+      filter(nest_id == .nest_id) %>%
+      left_join(
+        nest_query(.con),
+        by = "nest_id"
+      )
+  }
+
+# schedule ----------------------------------------------------------------
+
+schedule <-
+  function(.con = con, .week = NULL) {
+    days <- tbl(.con, "schedule_day")
+    
+    if (!is.null(.week)) {
+      days <-
+        days %>%
+        filter(week == .week)
+    }
+    
+    days %>%
+      select(
+        week,
+        date,
+        day,
+        arrive,
+        patch_order,
+        patch_count,
+        boards,
+        check_nests,
+        predator_cameras,
+        notes
+      ) %>%
+      arrange(
+        date,
+        patch_order
+      ) %>%
+      collect()
+  }
+
+# verification -------------------------------------------------------------
+
+# Nests discovered on or after a date:
+
+nests_since <-
+  function(.con = con, .date) {
+    nest_query(.con) %>%
+      filter(discovery_date >= .date) %>%
+      arrange(
+        discovery_date,
+        nest_id
+      )
+  }
+
+# Interval checks recorded on or after a date:
+
+checks_since <-
+  function(.con = con, .date) {
+    tbl(.con, "interval_check") %>%
+      filter(check_date >= .date) %>%
+      left_join(
+        nest_query(.con),
+        by = "nest_id"
+      ) %>%
+      arrange(
+        check_date,
+        nest_id
+      )
+  }
+
+# Row counts for the core tables (a check to see if new data were added):
+
+data_counts <-
+  function(.con = con) {
+    tables <-
+      c(
+        "nest",
+        "interval_check",
+        "gps_point",
+        "nest_substrate",
+        "track"
+      )
+    tibble(
+      table = tables,
+      rows =
+        map_int(
+          tables,
+          function(.t) {
+            tbl(.con, .t) %>%
+              tally() %>%
+              pull(n)
+          }
+        )
+    )
+  }
