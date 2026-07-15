7139c3f7f2bac48787bf10a92c6a7b8b173b9edd
Brian Evans <evansbr@si.edu>
2026-07-15 07:47:29 -0400

    trial render

--- outputs/nest_app_api/sw.js
+++ outputs/nest_app_api/sw.js
@@ -23,7 +23,7 @@
  */
 "use strict";
 
-var SW_VERSION = "v7";
+var SW_VERSION = "v8";
 var CACHE = "nestapp-shell-" + SW_VERSION;
 
 // With embed-resources: false the app is split across files. Precache the
