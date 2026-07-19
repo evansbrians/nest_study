484ed792c89b6d3a170e88362e159a9110e535b4
Brian Evans <evansbr@si.edu>
2026-07-19 12:46:04 -0400

    Delete zzz_emergency_revert.R (force-push revert foot-gun; recoverable from history)

--- zzz_emergency_revert.R
+++
@@ -1,9 +0,0 @@
-repo <- "."   # run from the repo root
-system(paste("rm -f", file.path(repo, ".git/index.lock")))
-cat("== reset ==\n")
-cat(system2("git", c("-C", repo, "reset", "--hard", "67ad1a9"), stdout=TRUE, stderr=TRUE), sep="\n")
-cat("\n== push ==\n")
-cat(system2("git", c("-C", repo, "push", "--force", "origin", "main"), stdout=TRUE, stderr=TRUE), sep="\n")
-cat("\n== head ==\n")
-cat(system2("git", c("-C", repo, "log", "-1", "--pretty=%h %ad %s", "--date=iso"), stdout=TRUE, stderr=TRUE), sep="\n")
-cat("\nDONE_EMERGENCY_REVERT_67ad1a9\n")
